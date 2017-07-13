3dd402d07ab550725457c22de44ec77f73cef2a2
rchaser53 <t.yoshizawa@sorabito.com>
2017-07-13 12:35:47 +0900

    fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	context: path.resolve(__dirname, './src'),
   devtool: 'inline-source-map',
   entry: {
-    index: "./connection/goodCode.js"
+    index: "./connection/good.js"
   },
   output: {
     path: path.resolve(__dirname, "dist"),
